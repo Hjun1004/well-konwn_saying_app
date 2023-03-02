8cbc7b2280ff9c7eae3ab782a1f70a4a8284afa4
Hjun1004 <hjun1004@naver.com>
2023-03-02 10:15:11 +0900

    5단계

--- src/main/java/org/example/명언앱_만들기/Main.java
+++ src/main/java/org/example/명언앱_만들기/Main.java
@@ -10,17 +10,28 @@ public class Main {
         int count=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 ==");
+
         while (true){
             System.out.print("명령) ");
             String sd = sc.nextLine();
-            if("등록".equals(sd)){
+
+            if("등록".equals(sd.trim())){
                 al.add(new Well_Known());
-                al.get(0).등록();
+                al.get(count).등록(count+1);
                 count++;
                 System.out.println(count + "번 명언이 등록되었습니다.");
-
             }
-            else if("종료".equals(sd)){
+
+            //System.out.println(al.get(0).getWell());
+
+            if("목록".equals(sd.trim())){
+                for(int i = count-1 ; i>=0 ; i--){
+                    System.out.printf("%d / %s / %s \n",al.get(i).getId(),al.get(i).getWriter(), al.get(i).getWell());
+                }
+            }
+
+
+            else if("종료".equals(sd.trim())){
                 break;
             }
         }
@@ -35,7 +46,7 @@ class Well_Known{
     private String writer;
     private String well;
 
-    private int count = 0;
+    //private int count = 0;
 
     public void setId(int id) {
         this.id = id;
@@ -61,11 +72,7 @@ class Well_Known{
         return well;
     }
 
-    public int getCount(){
-        return count;
-    }
-
-    void 등록(){
+    void 등록(int count){
         Scanner sc = new Scanner(System.in);
         System.out.print("명언 : ");
         String known = sc.nextLine();
@@ -73,7 +80,7 @@ class Well_Known{
         String name = sc.nextLine();
         this.well = known;
         this.writer = name;
-
+        this.id = count;
 
     }
 
